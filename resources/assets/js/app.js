fd356f508756881fdf0bf9524df723fc8ba7fbc8
felixrono <ronfello95@gmail.com>
2017-07-26 09:59:33 -0700

    nav menu setup

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,3 +12,7 @@ window.Vue = require('vue');
 import Buefy from 'buefy'
 
 Vue.use(Buefy);
+var app = new Vue({
+	el: '#app',
+	
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,62 +15,38 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+    <nav class="nav has-shadow">
+        <div class="container">
+            <div class="nav-left">
+                <a class="nav-item" href="#">
+                    <img src="{{ asset('images/devmarketer-logo.png')}}" alt="Logo">
+                </a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile">Discuss</a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile">Share</a>
             </div>
-        </nav>
 
+            <div class="nav-right">
+            @if(Auth::guest())
+                <a href="#" class="nav-item is-tab ">Login</a>
+                <a href="#" class="nav-item is-tab ">Join the Community</a>
+                @else
+                <button class="dropdown nav-item is-openis-tab">
+                    Hey Alex<span class="icon"> <i class="fa fa-caret-down"></i></span>
+
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Notification</a></li>
+                        <li><a href="#">Settings</a></li>
+                        <li class="separator"></li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </button>
+                @endif
+
+            </div>
+        </div>
+    </nav>
         @yield('content')
     </div>
 
