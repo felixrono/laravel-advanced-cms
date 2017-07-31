076457dbfab8b415aaeaa69ec5ff53318c38f4b1
felixrono <ronfello95@gmail.com>
2017-07-31 08:19:58 -0700

    Admin panel added, Manage users dashboard created

---
+++ resources/views/_includes/nav/main.blade.php
@@ -0,0 +1,32 @@
+<nav class="nav has-shadow">
+        <div class="container">
+            <div class="nav-left">
+                <a class="nav-item" href="">
+                <img src="{{asset('images/devmarketer-logo.png')}}">
+                </a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile">Discuss</a>
+                <a href="#" class="nav-item is-tab is-hidden-mobile">Share</a>
+
+            </div>
+
+            <div class="nav-right" style="overflow: visible;">
+                @if(Auth::guest())
+                <a href="{{route('login')}}" class="nav-item is-tab">Login</a>
+                 <a href="{{route('register')}}" class="nav-item is-tab">Join the Community</a>
+                 @else
+                 <button class="dropdown  nav-item is-tab">Welcome
+                     {{Auth::user()->name }}<span class="icon"><i class="fa fa-caret-down"></i></span>
+                     <ul class="dropdown-menu">
+                           <li><a href="#"> <span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i> </span> Profile</a></li>
+                           <li><a href="#"><span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i> </span>Notifications</a></li>
+                           <li><a href="{{ route('manage.dashboard') }}"> <span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i> </span>Manage</a></li>
+                           <li class="seperator"></li>
+                           <li><a href="#"> <span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i> </span>Logout</a></li>
+                     </ul>
+                 </button>
+                 @endif
+
+            </div>
+        </div>
+    </nav>
---
+++ resources/views/_includes/nav/manage.blade.php
@@ -0,0 +1,17 @@
+<div class="side-menu">
+	<aside class="menu">
+		<p class="menu-label m-t-30 m-l-10">
+			General
+		</p>
+		<ul class="menu-list">
+			<li> <a href="{{ route('manage.dashboard')}}">Dashboard </a></li>
+		</ul>
+		<p class="menu-label">
+			Administration
+		</p>
+		<ul class="menu-list">
+			<li><a href="{{ route('users.index') }}"> Manage Users</a> </li>
+			<li><a href="#"> Roles &amp; Permissions</a> </li>
+		</ul>
+	</aside>
+</div>
